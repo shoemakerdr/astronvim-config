a4bf7de25b561e93c4290b31e8f3930f98e1cda2
Derek Shoemaker <shoemakerdr@gmail.com>
2023-12-16 20:33:27 -0600

    Change colorscheme to kanagawa

--- init.lua
+++ init.lua
@@ -18,7 +18,7 @@ return {
   },
 
   -- Set colorscheme to use
-  colorscheme = "OceanicNext",
+  colorscheme = "kanagawa",
 
   -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
   diagnostics = {
--- plugins/user.lua
+++ plugins/user.lua
@@ -8,5 +8,32 @@ return {
 
   "ziglang/zig.vim",
   "mhartington/oceanic-next",
-  "rebelot/kanagawa.nvim",
+
+  {
+    "rebelot/kanagawa.nvim",
+    opts = {
+      compile = false,             -- enable compiling the colorscheme
+      undercurl = false,
+      commentStyle = { italic = true },
+      functionStyle = {},
+      keywordStyle = { italic = true},
+      statementStyle = { bold = true },
+      typeStyle = {},
+      transparent = false,         -- do not set background color
+      dimInactive = false,         -- dim inactive window `:h hl-NormalNC`
+      terminalColors = true,       -- define vim.g.terminal_color_{0,17}
+      colors = {                   -- add/modify theme and palette colors
+          palette = {},
+          theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
+      },
+      overrides = function(colors) -- add/modify highlights
+          return {}
+      end,
+      theme = "wave",              -- Load "wave" theme when 'background' option is not set
+      background = {               -- map the value of 'background' option to a theme
+          dark = "wave",           -- try "dragon" !
+          light = "lotus"
+      }
+    }
+  },
 }
