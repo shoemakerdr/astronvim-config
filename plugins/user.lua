53ac7b06145ff42ac2aaeaaf6c677b2d4d184bfd
Derek Shoemaker <shoemakerdr@gmail.com>
2023-12-16 19:20:00 -0600

    Added some plugins from mac mini

--- plugins/user.lua
+++ plugins/user.lua
@@ -1,12 +1,12 @@
 return {
-  -- You can also add new plugins here as well:
-  -- Add plugins, the lazy syntax
-  -- "andweeb/presence.nvim",
-  -- {
-  --   "ray-x/lsp_signature.nvim",
-  --   event = "BufRead",
-  --   config = function()
-  --     require("lsp_signature").setup()
-  --   end,
-  -- },
+
+  {
+    "kylechui/nvim-surround",
+    lazy = false,
+    config = function() require("nvim-surround").setup() end,
+  },
+
+  "ziglang/zig.vim",
+  "mhartington/oceanic-next",
+  "rebelot/kanagawa.nvim",
 }
